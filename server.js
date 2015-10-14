aa4be173abb8714ee0c2f020a7db681b1562b2a5
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2015-10-14 14:52:50 +0530

    added version number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express()
 var port = process.env.PORT || 1337;
 app.use('/', function(req, res, next) {
-    res.send('Hello World')
+    res.send('Hello World' +  process.version)
 })
 
 app.listen(port, function() {
